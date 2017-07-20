6a6882a647e044f5ba7f3acc85a3ddef92b7d549
kt <215849121@qq.com>
2017-07-20 17:57:17 +0800

    1.增加管理员信息编辑页面 2.增加添加管理员按钮

--- adminpage/admin-manage.php
+++ adminpage/admin-manage.php
@@ -12,8 +12,9 @@
     //定义变量标识符，防止inc、func文件恶意调用
     define('LT_req',true);
     
-    require dirname(__FILE__).'./../function/login-register.func.php';
-    require dirname(__FILE__).'./../include/mysql.inc.php';
+//     require dirname(__FILE__).'./../function/login-register.func.php';
+//     require dirname(__FILE__).'./../include/mysql.inc.php';
+       require dirname(__FILE__).'./../function/edit-info.func.php';
     
     if(!isset($_COOKIE['LT_uid']))
     {
@@ -25,19 +26,71 @@
     {
         echo "<script language=javascript>location.href='../index.php';</script>";
     }
+    
+    if(isset($_GET['act']))
+    {
+        if(!isset($_GET['code']) || ($_GET['code'] != $_SESSION['identitycode']))
+        { 
+            _jumplocation('admin-manage.php');
+            exit;
+        }
+        
+        /***************************************************删除用户********************************************************/
+        if($_GET['act']=='delete')
+        {
+            $_deleted_mysql_fetch = mysql_fetch_assoc(mysql_query("SELECT * FROM `user_info` WHERE `user_uid` LIKE '{$_GET['value']}'"));
+            if(isset($_deleted_mysql_fetch['user_uid']) && $_deleted_mysql_fetch['user_uid'] != null)
+            {
+                $_deleted_mysql_fetch['user_delete_time'] = date("Y-m-d H:i:s",time()+8*60*60);
+                @mysql_query("INSERT INTO `user_deleted`(`user_id`,`user_uid`,`user_phone`,`user_pwd`,`user_rts`,`user_rt`,`user_email`,`user_name`,`user_jurisdiction`,`user_deleted_time`)
+                    VALUES('{$_deleted_mysql_fetch['user_id']}','{$_deleted_mysql_fetch['user_uid']}','{$_deleted_mysql_fetch['user_phone']}',
+                    '{$_deleted_mysql_fetch['user_pwd']}','{$_deleted_mysql_fetch['user_rts']}','{$_deleted_mysql_fetch['user_rt']}','{$_deleted_mysql_fetch['user_email']}',
+                    '{$_deleted_mysql_fetch['user_name']}','{$_deleted_mysql_fetch['user_jurisdiction']}','{$_deleted_mysql_fetch['user_delete_time']}')") or die('数据库执行错误:'.mysql_error().print_r($_deleted_mysql_fetch));
+                
+                mysql_query("DELETE FROM `user_info` WHERE `user_info`.`user_uid` = '{$_GET['value']}'");
+                _alert_back('删除成功');
+            }
+            else
+            {
+                _alert_back('用户不存在');
+            }
+        }
+        /*******************************************************************************************************************/
+        
+        /***************************************************编辑用户信息********************************************************/
+        else if($_GET['act'] == 'edit')
+        {
+            $_edit_mysql_fetch = mysql_fetch_assoc(mysql_query("SELECT * FROM `user_info` WHERE `user_uid` LIKE '{$_GET['value']}'"));
+            $_baseinfo_mysql_fetch = mysql_fetch_assoc(mysql_query("SELECT * FROM `user_baseinfo` WHERE `user_uid` LIKE '{$_GET['value']}'"));
+            if(isset($_GET['func']))
+            {
+                switch ($_GET['func'])
+                {
+                    case 'editlogininfo':_checkupdata_editlogininfo($_POST,$_edit_mysql_fetch);break;
+                    case 'editpwd':_checkupdata_editpwd($_POST['editpassword'],$_POST['editpassword_confirm'],$_edit_mysql_fetch);break;
+                        
+                }
+                _aler_jump('操作成功', 'admin-manage.php');
+            } 
+        }
+        /*******************************************************************************************************************/
+    }
+
+
+    $_SESSION['identitycode'] = md5(mt_rand());//生成页面唯一标识码
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no"/>
-<title>管理系统-管理员</title>
+<title>管理系统-平台管理员</title>
 
 <link href="../bootstrap-3.3.5-dist/css/bootstrap.min.css" title="" rel="stylesheet" />
 <link title="" href="../css/style.css" rel="stylesheet" type="text/css"  />
 <link title="blue" href="../css/dermadefault.css" rel="stylesheet" type="text/css"/>
 <link href="../css/templatecss.css" rel="stylesheet" title="" type="text/css" />
-<link rel="shortcut icon" href="../icon/pooh.ico" />
+<link rel="shortcut icon" href="../icon/pooh.ico" />	
 <!-- <script src="script/jquery-1.11.1.min.js" type="text/javascript"></script> -->
 <!-- <script src="script/jquery.cookie.js" type="text/javascript"></script> -->
 <script src="../bootstrap-3.3.5-dist/js/bootstrap.min.js" type="text/javascript"></script>
@@ -111,7 +164,58 @@
 
                
                <!-------------------------------------------信息主体页面----------------------------------------------------------->
-               <div class="row newslist" style="margin:30px 0 0 50px ;width:2000px;">
+               <!----------------------------------------编辑用户信息------------------------------------------------------>
+
+               <!----------------------------------------编辑管理员信息------------------------------------------------------>
+               <?php if(isset($_GET['act']) && $_GET['act'] == 'edit'){ ?>
+                   <div style='width:1200px;'>
+                       <form name="editlogininfo" method="post" style='margin:50px 0 0 100px;width:auto;border:1px solid #bbbbbb;'
+                             action="admin-manage.php?act=edit&func=editlogininfo&value=<?php echo $_edit_mysql_fetch['user_uid'];?>&code=<?php echo $_SESSION['identitycode'];?>" >
+                       <h4 style ="font-size:20px;" align="center"><strong>登陆信息</strong></h4>
+                                <p style ="font-size:18px;margin:20px 0 10px 0;"> 
+                                    <label for="edituserid" style='margin-left:50px;width:100px;'>管理员编号:</label>
+                                    <span id="edituserid" style ="font-size:18px;"><?php echo $_edit_mysql_fetch['user_id']; ?></span>
+                                </p>
+                                <p style ="font-size:18px;margin:20px 0 10px 0;"> 
+                                    <label for="editusername"  style='margin-left:50px;width:100px;'>管理员名称:</label>
+                                    <input id="editusername" name="editusername" required="required" type="text" style='width:350px;'
+                                           value=<?php echo $_edit_mysql_fetch['user_name']; ?> />
+                                </p>
+                                <p style ="font-size:18px;margin:20px 0 10px 0;">           
+                                    <label for="editemail"  style='margin-left:50px;width:100px;'>邮 箱:</label>
+                                    <input id="editemail" name="editemail" required="required" type="email"  style='width:350px;'
+                                           value=<?php echo $_edit_mysql_fetch['user_email']; ?> /> 
+                                </p>
+                                <p style ="font-size:18px;margin:20px 0 10px 0;">          
+                                    <label for="editphonenum"  style='margin-left:50px;width:100px;'>联系电话:</label>
+                                    <input id="editphonenum" name="editphonenum" required="required" type="text" style='width:350px;'
+                                           value=<?php echo $_edit_mysql_fetch['user_phone']; ?> />
+                                    <input type="submit" value="保存" style="width:100px;float:right; margin-right:30px;"/> 
+                                </p>
+                       </form>
+
+                       <form name="editpwd" method="post" style="margin:30px 0 0 100px ;width:auto;border:1px solid #bbbbbb;"
+                             action="admin-manage.php?act=edit&func=editpwd&value=<?php echo $_edit_mysql_fetch['user_uid'];?>&code=<?php echo $_SESSION['identitycode'];?>">
+                       <h4 style ="font-size:20px;" align="center"><strong>密码修改</strong></h4>
+                                <input type="hidden" name="edituserid" value=<?php echo $_edit_mysql_fetch['user_uid'];?> />
+                                <p style ="font-size:18px;margin:20px 0 10px 0;"> 
+                                    <label for="editpassword" class="youpasswd" style='margin-left:50px;width:100px;'>密 码：</label>
+                                    <input id="editpassword" name="editpassword" required="required" type="password" style='width:350px;'/>
+                                </p>
+                                <p style ="font-size:18px;margin:20px 0 10px 0;"> 
+                                    <label for="editpassword_confirm" class="youpasswd" style='margin-left:50px;width:100px;'>确认密码：</label>
+                                    <input id="editpassword_confirm" name="editpassword_confirm" required="required" type="password" style='width:350px;'/>
+                                    <input type="submit" value="保存" style="width:100px;float:right; margin-right:30px;"/>
+                                </p>
+                      </form>
+                      <div style="text-align: center;margin:10px 0 50px 0;"><a href="admin-manage.php" style="font-size: 18px;">【返回】</a></div>
+                      
+               </div>
+               <!--------------------------------------------------------------------------------------------------->
+
+               <!----------------------------------------用户信息显示------------------------------------------------------>
+               <?php }else if(!isset($_GET['act'])){?>
+               <div class="row newslist" style="margin:30px 0 0 50px ;width:1800px;">
                   <div class="col-md-8">
                     <div class="panel panel-default"> 
                          <div class="panel-body">
@@ -123,7 +227,14 @@
                          <div class="w20 pull-left text-center"><strong>添加时间</strong></div>
                          <div class="w10 pull-left text-center"><strong>操作</strong></div></div>
                       <?php   
-                        $_i = mysql_fetch_assoc(mysql_query("select max(user_id) from user_info"))['max(user_id)'];
+                        if(isset($_GET['show']) && $_GET['show']=='all')
+                        {
+                            $_i = mysql_fetch_assoc(mysql_query("select max(user_id) from user_info"))['max(user_id)'];
+                        }
+                        else
+                        {
+                            $_i = 10;
+                        } 
                         $num = 1;
                         for($_j=1;$_j<=$_i;$_j++)
                         {
@@ -137,21 +248,32 @@
                                 <div class="w20 pull-left text-center"><?php echo $_mysql_fetch['user_email']; ?></div>
                                 <div class="w20 pull-left text-center"><?php echo $_mysql_fetch['user_phone']; ?></div>
                                 <div class="w20 pull-left text-center"><?php echo $_mysql_fetch['user_rt']; ?></div>
-                                <div class="text-center\">
-                                    <a href="admin-manage.php?act=edit&value=<?php echo $_mysql_fetch['user_uid']; ?>"><button>编辑</button></a>
-                                    <a href="admin-manage.php?act=delete&value=<?php echo $_mysql_fetch['user_uid']; ?>"><button>删除</button></a>
+                                <div class="text-center" >
+                                    <a style='text-decoration:none;' href="admin-manage.php?act=edit&value=<?php echo $_mysql_fetch['user_uid'];?>&code=<?php echo $_SESSION['identitycode'];?>">
+                                        <button>编辑</button>
+                                    </a>
+                                    <a href="javascript:if(confirm('确实要删除该用户吗?'))location='admin-manage.php?act=delete&value=<?php echo $_mysql_fetch['user_uid']; ?>&code=<?php echo $_SESSION['identitycode']; ?>'">
+                                        <button>删除</button>
+                                    </a>
                                 </div>
                             </div>
                        <?php } ?>
+                            <div style="text-align: center;margin:50px 0 20px 0;"><a href="admin-manage.php?act=add-manage&code=<?php echo $_SESSION['identitycode'];?>"
+                                                                                     style="font-size: 18px;"><button>添加管理员</button></a></div>
+                       <?php 
+                        } 
+                        mysql_close();
+                       ?>
                        
                     </div>
                   </div>
-               </div>
-                 <!------------------------------------------------------------------------------------------------------------------->
-               </div>
+                </div>
+              </div>
+              <!------------------------------------------------------------------------------------------------------------------->
             </div>
        </div>			
 	 </div>
 </body>
 </html>
+
 
